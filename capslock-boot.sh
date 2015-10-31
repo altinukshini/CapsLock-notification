8bcf03b70e746d81f45f08e7c6273d02cf0ff356
Altin Ukshini <altin.ukshini@gmail.com>
2015-10-31 20:33:24 +0100

    Create capslock-boot.sh

---
+++ capslock-boot.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+DISB_RELEASE=$(lsb_release -r | awk '{print $2}')
+
+notify() {
+  USER=$(who | sed -n '/ (:0[\.0]*)$\| :0 /{s/ .*//p;q}')
+  USERCNT=$(who | wc -l)
+  if [ ! "$(whoami)" = "$USER" ]; then
+    if [ ! "$USERCNT" -lt 1 ]; then
+        su $USER -l -c "DISPLAY=:0.0 -t 1000 --hint int:transient:1 -i /usr/share/icons/gnome/scalable/devices/input-keyboard-symbolic.svg \"$1\">/dev/null"
+    fi
+  else
+    if [ ! "$USERCNT" -lt 1 ]; then
+        /usr/bin/notify-send -t 1000 --hint int:transient:1 -i /usr/share/icons/gnome/scalable/devices/input-keyboard-symbolic.svg "$1" > /dev/null
+    fi
+  fi
+}
+
+NOTIFICATION_STATE=$(xset q | grep "Caps Lock:" | awk '{print $4}' | grep -c on)
+
+while [ true ]; do
+
+    CAPS_STATE=$(xset q | grep "Caps Lock:" | awk '{print $4}' | grep -c on)
+
+    if [ $NOTIFICATION_STATE -eq 0 ]; then
+        if  [ "$CAPS_STATE" -eq 0 ]; then
+            notify "Caps Lock OFF"
+            NOTIFICATION_STATE=1
+        fi
+    elif [ $NOTIFICATION_STATE -eq 1 ]; then
+        if [ "$CAPS_STATE" -eq 1 ]; then
+            notify "Caps Lock ON"
+            NOTIFICATION_STATE=0
+            paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
+        fi
+    fi
+
+    sleep 0.5
+
+done
